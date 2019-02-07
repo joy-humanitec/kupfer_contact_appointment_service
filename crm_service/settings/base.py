de0813cd68fd44ced93ff8da2502e5302455c294
Rafael Muñoz Cárdenas <rafael@humanitec.com>
2019-02-07 09:54:04 +0100

    PLAT-254- Make SECRET_KEY mandatory

--- crm_service/settings/base.py
+++ crm_service/settings/base.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'on-z!b^sr*4+h@w!55p4bwnmdcea!)80u#)iaoi4!5^*_z9+%r'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False if os.getenv('DEBUG') == 'False' else True
